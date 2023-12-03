539721da7512bb5656dc569766b0a84d0aa5cff7
Robyn F H Veitch <oddert@gmail.com>
2023-12-03 14:38:36 +0000

    finished day 1. added more config & utils

--- 1/index.py
+++ 1/index.py
@@ -1,13 +1,17 @@
 import sys
 import re
+import os
 
-sys.path.append(r'D:\Users\robyn\code\advent-of-code-2023')
+# Needed to import from folder directory. Replace with your own project.
+# Probably a better way to do this that doesn't involve revealing my own file structure but whatever, you're not my mum. 
+sys.path.append(r'C:\dev\advent-of-code-2023')
+
+from config import project_dir
 
 from utils.read_input import read_input
+from utils.string_utils import reverse_string
 
 text = read_input('./1/input.txt')
-
-print(text)
 
 def get_pairing(num_list):
 	if len(num_list) == 0:
@@ -47,22 +51,30 @@ matches = {
 part2_coords = []
 part2_total = 0
 
+def parse_search(matcher, line, reverse_match=False):
+	print('Matching ', matcher, line)
+	matched_digits = re.findall(matcher, line)
+	print('Matched Digits: ', matched_digits)
+	parsed_digits = []
+	for digit in matched_digits:
+		if reverse_match:
+			digit = reverse_string(digit)
+		if digit in matches:
+			parsed_digits.append(matches[digit])
+		else:
+			parsed_digits.append(digit)
+	print('Parsed Digits: ', parsed_digits)
+	return parsed_digits
+
 for line in text:
 	if line == '':
 		continue
 	line = line[:-1]
-	print('line', line)
-	str_digits = re.findall('one|two|three|four|five|six|seven|eight|nine|[0-9]', line)
-	print('str_digits', str_digits)
-	parsed_digits = []
-	for str_digit in str_digits:
-		if str_digit in matches:
-			parsed_digits.append(matches[str_digit])
-		else:
-			parsed_digits.append(str_digit)
-	print('parsed_digits', parsed_digits)
-	print('paring', get_pairing(parsed_digits))
-	part2_coords.append(get_pairing(parsed_digits))
+	print('# line', line)
+	forward_matches = parse_search('one|two|three|four|five|six|seven|eight|nine|[0-9]', line)
+	reversed_matches = parse_search(f"{reverse_string('one|two|three|four|five|six|seven|eight|nine')}|[0-9]", reverse_string(line), True)
+
+	part2_coords.append(get_pairing([forward_matches[0], reversed_matches[0]]))
 
 print(part2_coords)
 for coord in part2_coords:
@@ -70,6 +82,3 @@ for coord in part2_coords:
 
 print('Part 1 Total: ', part1_total)
 print('Part 2 Total: ', part2_total)
-
-
-print(re.findall('enin|thgie|neves|xis|evif|ruof|eerht|owt|eno|[0-9]', ''.join(list('twone49twone')[::-1])))
